4a07aa29c7fc470ab85122bf7071ca7c442cce24
Jameson Proctor <jameson@jp1971.com>
2014-03-23 13:27:02 -0400

    Correcting database table name.

--- uninstall.php
+++ uninstall.php
@@ -5,7 +5,7 @@ if ( defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	// Delete custom table
 	global $wpdb;
 
-	$table_name = $wpdb->prefix . 'rotate_html';
+	$table_name = $wpdb->prefix . 'roto_text';
 	$sql = 'DROP TABLE IF EXISTS '.$table_name;
 	$wpdb->query( $sql );
 }
